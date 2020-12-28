06f2bfae68bf40dcb8e5eeb4da46682d91253d2c
Renaud-HUSSON <renaudhusson09@gmail.com>
2020-12-28 01:48:12 +0100

    📈 Google Analytics

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -6,9 +6,19 @@ import Projet from "./pages/Projet"
 import Projets from "./pages/Projets"
 import {AnimatePresence} from 'framer-motion'
 import Login from "./pages/Login"
+import ReactGA from 'react-ga';
 
 const Routes = () => {
   const location = useLocation()
+
+  ReactGA.initialize(process.env.REACT_APP_GA, {
+    debug: false,
+    titleCase: false,
+    gaOptions: {
+      siteSpeedSampleRate: 100
+    }
+  })
+  ReactGA.pageview(window.location.pathname + window.location.search);
   
   return <AnimatePresence exitBeforeEnter>
     <Switch location={location} key={location.pathname}>
